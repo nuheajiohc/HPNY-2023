ded0129018c1e85e66d2b9aa53b5dd1250841ff8
nuheajiohc <jaehun341012@gmail.com>
2023-01-22 22:24:18 +0900

    faet: 로컬 서버 구축

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 9600;
+
+app.use("/", express.static(__dirname));
+
+app.get("/*", (req, res) => res.sendFile(__dirname + "/index.html"));
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
