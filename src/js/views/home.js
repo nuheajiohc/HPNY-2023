1bdfd55c2e915a32af5d0635a1fac911301a3a7e
nuheajiohc <jaehun341012@gmail.com>
2023-02-01 15:45:44 +0900

    refactor: 메인 페이지 리팩토링

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,29 +1,18 @@
 import { $ } from "../utils/dom.js";
 import PostApi from "../api/postApi.js";
+import header from "./header.js";
 
-export default function Home() {
-  document.title = "HPNY 2023";
-  this.render = async () => {
-    $("#nav").innerHTML = `
-        <a href="/" id="backIcon-wrapper" class="hidden">
-          <img src="../src/images/left-arrow.png" />
-        </a>
-        <a href="/" id="nav-title">HPNY 2023</a>
-    `;
-    $("section").setAttribute("id", "home-section");
-    $("section").innerHTML = `
-      <div id="create-post-btn-wrap">
-        <a href="/upload" id="create-post-btn">게시글 작성하기</a>
-      </div>
-      <ul id="post-list">${await updatePostList()}
-      </ul>`;
-  };
+export default class Home {
+  constructor() {
+    document.title = "HPNY 2023";
+    header();
+    $("#backIcon-wrapper").className = "hidden";
+  }
 
-  const updatePostList = async () => {
-    let template = "";
+  async getPostList() {
     const postApi = new PostApi();
     const postList = await postApi.getPostList();
-    postList.forEach(post => {
+    return postList.reduce((template, post) => {
       const { postId, title, content, image } = post;
       template += `
       <li id="post">
@@ -34,17 +23,19 @@ export default function Home() {
             <p>${content}</p>
           </div>
         </a>
-      </li>`;
-    });
-    return template;
-  };
+      </li>
+      `;
+      return template;
+    }, "");
+  }
+
+  async render() {
+    $("section").setAttribute("id", "home-section");
+    $("section").innerHTML = `
+      <div id="create-post-btn-wrap">
+        <a href="/upload" id="create-post-btn">게시글 작성하기</a>
+      </div>
+      <ul id="post-list"></ul>`;
+    $("#post-list").innerHTML = await this.getPostList();
+  }
 }
-
-// class Home{
-//   constructor(){
-//     document.title = "HPNY 2023";
-//   }
-//   view(){
-
-//   }
-// }
