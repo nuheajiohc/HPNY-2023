aa4013cc47c2340f853691aaf6431a2862f65b78
nuheajiohc <jaehun341012@gmail.com>
2023-02-01 15:52:35 +0900

    refactor: 업로드 페이지 리팩토링

--- src/js/views/uploadPost.js
+++ src/js/views/uploadPost.js
@@ -1,14 +1,13 @@
 import { $ } from "../utils/dom.js";
+import header from "./header.js";
 
-export default function UploadPost() {
-  document.title = "HPNY 2023 | upload post";
-  this.render = () => {
-    $("#nav").innerHTML = `
-    <a href="/" id="backIcon-wrapper">
-      <img src="../src/images/left-arrow.png" />
-    </a>
-    <a href="/" id="nav-title">HPNY 2023</a>
-`;
+export default class UploadPost {
+  constructor() {
+    document.title = "HPNY 2023 | upload post";
+    header();
+  }
+
+  render() {
     $("section").setAttribute("id", "upload-section");
     $("section").innerHTML = `
       <div id="img-add-wrapper">랜덤 이미지 추가하기</div>
@@ -16,5 +15,5 @@ export default function UploadPost() {
       <textarea placeholder="글 내용을 작성해주세요." id="textarea-content" maxlength="500"></textarea>
       <button id="submit-btn" disabled="">글 작성하기</button>
     `;
-  };
+  }
 }
