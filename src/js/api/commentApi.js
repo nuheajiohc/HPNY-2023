848716fdabddddbf76b5ec98584b63d40afe84d0
nuheajiohc <jaehun341012@gmail.com>
2023-02-07 22:26:54 +0900

    fix: 댓글기능 오류 수정

--- src/js/api/commentApi.js
+++ src/js/api/commentApi.js
@@ -2,17 +2,20 @@ import { BASE_URL, END_POINT } from "/src/js/utils/url.js";
 
 export default class CommentApi {
   async addComment(postNumber, contentValue) {
-    return await fetch(BASE_URL + END_POINT.COMMENT + postNumber.split("/")[2], {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        content: contentValue,
-      }),
-    });
+    return await fetch(
+      "/" + BASE_URL + END_POINT.COMMENT + postNumber.split("/")[2],
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          content: contentValue,
+        }),
+      },
+    );
   }
 
   async deleteComment(commentId) {
-    return await fetch(BASE_URL + END_POINT.COMMENT + commentId, {
+    return await fetch("/" + BASE_URL + END_POINT.COMMENT + commentId, {
       method: "DELETE",
     }).then(res => res.json());
   }
