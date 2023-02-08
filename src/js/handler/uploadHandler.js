dd6c4e1cd68bb777de2443a18b4cf71b1d1a41b8
nuheajiohc <jaehun341012@gmail.com>
2023-02-08 19:31:40 +0900

    fix: 이미지추가 오류 수정

--- src/js/handler/uploadHandler.js
+++ src/js/handler/uploadHandler.js
@@ -4,24 +4,31 @@ import imageApi from "/src/js/api/imageApi.js";
 import PostApi from "/src/js/api/postApi.js";
 
 let imageUrl;
-let clickedImageAdd = false;
 export default async function handleUpload(e) {
-  if (e.target.id === "img-add" && !clickedImageAdd) {
+  if (
+    e.target.id === "img-add" &&
+    $("#img-add").innerHTML === "랜덤 이미지 추가하기"
+  ) {
     imageUrl = await imageApi();
     $("#img-add").innerHTML = `<img src=${imageUrl}>`;
-    clickedImageAdd = true;
   }
 
   if (e.target.id === "submit-btn") {
     const postApi = new PostApi();
-    await postApi.addPost($("#input-title").value, $("#textarea-content").value, imageUrl);
+    await postApi.addPost(
+      $("#input-title").value,
+      $("#textarea-content").value,
+      imageUrl,
+    );
     window.history.pushState({}, "", "/");
     const route = new Route();
     route.loadPage();
-    clickedImageAdd = false;
     return;
   }
 
-  const isPossibleSubmit = $("#input-title").value && $("#textarea-content").value && clickedImageAdd;
+  const isPossibleSubmit =
+    $("#input-title").value &&
+    $("#textarea-content").value &&
+    $("#img-add").innerHTML !== "랜덤 이미지 추가하기";
   $("#submit-btn").disabled = !isPossibleSubmit;
 }
